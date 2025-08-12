930b512ba425fc6333fcbd931e3c58319deadc69
SaumyaSingh04 <saumya0419@gmail.com>
2025-08-12 15:55:51 +0530

    add url

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://backend-hazel-xi.vercel.app",
   "https://buddha-crm.vercel.app",
+  "https://buddha-admin.vercel.app"
 ];
 app.use(
   cors({
